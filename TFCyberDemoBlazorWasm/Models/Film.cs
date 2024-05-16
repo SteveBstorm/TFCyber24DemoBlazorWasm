edc4f431a9c0734e3e5a1d3cbec24027a374b01b
stevebstorm <steve.lorent@gmail.Com>
2024-05-16 11:24:14 +0200

    commitage

---
+++ TFCyberDemoBlazorWasm/Models/Film.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TFCyberDemoBlazorWasm.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        [MinLength(10)]
+        public string Description { get; set; }
+        [Range(1977, int.MaxValue)]
+        public int AnneeDeSortie { get; set; }
+    }
+}
---
+++ TFCyberDemoBlazorWasm/Pages/Demo4.razor.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+using TFCyberDemoBlazorWasm.Models;
+
+namespace TFCyberDemoBlazorWasm.Pages
+{
+    public partial class Demo4
+    {
+        public Film MyFormObject { get; set; }
+
+        protected override void OnInitialized()
+        {
+            MyFormObject = new Film();
+        }
+
+        public void ValidationFormulaire()
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(MyFormObject));
+        }
+    }
+}
