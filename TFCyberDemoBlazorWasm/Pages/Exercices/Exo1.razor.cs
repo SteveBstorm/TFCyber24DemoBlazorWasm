a0d727cc669d099b5d578d9077ee3ab59d9600b5
stevebstorm <steve.lorent@gmail.Com>
2024-05-16 09:49:37 +0200

    Correctif Exo quizz

---
+++ TFCyberDemoBlazorWasm/Pages/Exercices/Exo1.razor.cs
@@ -0,0 +1,25 @@
+﻿namespace TFCyberDemoBlazorWasm.Pages.Exercices
+{
+    public partial class Exo1
+    {
+        public string Prenom { get; set; }
+        List<string> Reponses { get; set; }
+        private bool gameOver;
+
+        public bool GameOver
+        {
+            get { return Reponses.Count == 3; }
+        }
+
+
+        protected override void OnInitialized()
+        {
+            Reponses = new List<string>();
+        }
+
+        public void EnregistrerReponse(string reponse)
+        {
+            Reponses.Add(reponse);
+        }
+    }
+}
---
+++ TFCyberDemoBlazorWasm/Pages/Exercices/Quizz.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TFCyberDemoBlazorWasm.Pages.Exercices
+{
+    public partial class Quizz
+    {
+        [Parameter]
+        public string Prenom { get; set; }
+        [Parameter]
+        public EventCallback<string> Repondre { get; set; }
+
+        public List<string> Questions { get; set; }
+        public int Counter { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Questions = new List<string>();
+            Questions.Add("Avez vous bien mangé à midi ?");
+            Questions.Add("Pas trop du Blazor ?");
+            Questions.Add("Vous en voulez plus ?");
+            Counter = 0;
+        }
+
+        public void ValiderReponse(string rep)
+        {
+            Repondre.InvokeAsync(rep);
+            Counter++;
+        }
+    }
+}
