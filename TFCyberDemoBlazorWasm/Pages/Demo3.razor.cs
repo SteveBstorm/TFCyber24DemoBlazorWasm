615604ebd5f3c67b0df0ef756543980c2b9d178d
stevebstorm <steve.lorent@gmail.Com>
2024-05-16 11:04:22 +0200

    Virtualize

---
+++ TFCyberDemoBlazorWasm/Pages/Demo3.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace TFCyberDemoBlazorWasm.Pages
+{
+    public partial class Demo3
+    {
+        public List<Guid> MaSuperListe { get; set; }
+
+        protected override void OnInitialized()
+        {
+            MaSuperListe = new List<Guid>();
+            for(int i = 0; i < 50; i++)
+            {
+                MaSuperListe.Add(Guid.NewGuid());
+            }
+        }
+    }
+}
